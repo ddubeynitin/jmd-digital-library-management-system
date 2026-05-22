dd26649fc6b43ae7fe5334b1194529378f49e388
Nitin Dubey <ddubeynitin30@gmail.com>
2026-05-22 15:31:55 +0530

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <div className="text-red-500"></div>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
